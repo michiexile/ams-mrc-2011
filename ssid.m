d9764728e6b034c2c47c8a6a0d6c9f3bb32e2872
Mikael Vejdemo-Johansson <mik@stanford.edu>
2011-06-21 14:29:33 -0600

    Working with witness complexes

--- ssid.m
+++ ssid.m
@@ -1,12 +1,14 @@
 import edu.stanford.math.plex4.*;
 
-maxV = 10.0;
+maxV = 50.0;
 maxD = 2;
 
 data = dlmread('grouppoints.csv');
 ms = api.Plex4.createEuclideanMetricSpace(data);
-vr = api.Plex4.createVietorisRipsStream(ms,maxD,maxV,100);
+ls = api.Plex4.createMaxMinSelector(ms,50);
+vr = api.Plex4.createWitnessStream(ls,maxD,maxV,100);
 vr.getSize()
+%%
 p = api.Plex4.getDefaultSimplicialAlgorithm(maxD);
 i = p.computeIntervals(vr);
 i = vr.transform(i);
